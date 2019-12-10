f07ee3c001ddcf8b001b457cf70c2129a73635f6
Jacob Vinzent <jacob_vinzent@hotmail.com>
2019-12-10 15:15:11 +0100

    Add project files.

---
+++ oidc_dotNet/login.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="oidc_dotNet.login" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="~/style.css" rel="stylesheet" />
+</head>
+<body runat="server">
+   
+        <form id="form1" runat="server">
+   
+        <div class="container">
+     <div class="loading"><div class="loader">
+    </div>
+    <%=User.Identity.Name%>
+         <asp:TextBox ID="TextBox1" runat="server" Height="152px" Width="918px"></asp:TextBox>
+        </form>
+    
+</body>
+</html>
---
+++ oidc_dotNet/login.aspx.cs
@@ -0,0 +1,249 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Net;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace oidc_dotNet
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            startLogin();
+
+        }
+
+
+      
+        private void startLogin()
+        {
+            PrincipalContext pc = null;
+            UserPrincipal principal = null;
+            string domain = "";
+            string ignoreSSLError = "";
+            string qseok_url = "";
+            string path = "";
+            string ignoreGroups = "";
+            List<string> ignoreGroupsArray = null;
+            string onlyGroupsStartingWith = "";
+
+
+
+            try
+            {
+                domain = ConfigurationManager.AppSettings["domain"];
+                ignoreSSLError = ConfigurationManager.AppSettings["ignoreSSLError"];
+                qseok_url = ConfigurationManager.AppSettings["qseok_url"];
+                path = ConfigurationManager.AppSettings["path"];
+                ignoreGroups = ConfigurationManager.AppSettings["ignoreGroups"];
+                onlyGroupsStartingWith = ConfigurationManager.AppSettings["onlyGroupsStartingWith"];
+
+                ignoreGroupsArray = ignoreGroups.Split(',').ToList();
+
+
+                GenericPrincipal genericPrincipal = GetGenericPrincipal();
+
+                var username = Thread.CurrentPrincipal.Identity.Name;
+
+                var username1 = HttpContext.Current.User.Identity.Name;
+
+
+                pc = new PrincipalContext(ContextType.Domain, domain);
+
+
+                if (pc is null)
+                {
+                    TextBox1.Text = "PC is null";
+                    return;
+                }
+
+                TextBox1.Text = genericPrincipal.Identity.Name;
+
+
+                principal = UserPrincipal.FindByIdentity(pc, genericPrincipal.Identity.Name);
+
+
+                TextBox1.Text = principal.Name;
+
+                var firstName = principal.GivenName ?? string.Empty;
+                var lastName = principal.Surname ?? string.Empty;
+                var groups = principal.GetGroups();
+
+                string[] groupArray = { };
+
+
+                foreach (GroupPrincipal g in groups)
+                {
+                    if (!ignoreGroupsArray.Contains(g.ToString()))
+                    {
+                        if (string.IsNullOrEmpty(onlyGroupsStartingWith) || (g.ToString().StartsWith(onlyGroupsStartingWith) && !string.IsNullOrEmpty(onlyGroupsStartingWith)))
+                        {
+                            //if (string.IsNullOrEmpty(groupArray))
+                            //{
+                            Array.Resize(ref groupArray, groupArray.Length + 1);
+                            groupArray[groupArray.GetUpperBound(0)] = g.ToString();
+                            //groupArray = "\"" + g.ToString() + "\"";
+                            //}
+                            //else
+                            //{
+                            //    Array.Resize(ref groupArray, groupArray.Length + 1);
+                            //    groupArray[groupArray.GetUpperBound(0)] = g.ToString();
+                            //}
+                        }
+                    }
+
+
+                }
+
+                string jwt = createJWT(genericPrincipal.Identity.Name, groupArray, firstName + " " + lastName);
+                string ticket = getTicket(jwt, ignoreSSLError.ToLower(), qseok_url, path);
+                JObject o = JObject.Parse(ticket);
+                JObject o1 = JObject.Parse(o.ToString());
+                string url = qseok_url + "?qlikticket=" + o1["Ticket"].ToString();
+                Response.Redirect(url);
+
+            }
+            catch (Exception ex)
+            {
+                TextBox1.Text = ex.Message;
+            }
+            finally
+            {
+                if (principal != null) principal.Dispose();
+                if (pc != null) pc.Dispose();
+            }
+        }
+
+        // Create a generic principal based on values from the current
+        // WindowsIdentity.
+        private static GenericPrincipal GetGenericPrincipal()
+        {
+            // Use values from the current WindowsIdentity to construct
+            // a set of GenericPrincipal roles.
+            WindowsIdentity windowsIdentity = WindowsIdentity.GetCurrent();
+            string[] roles = new string[10];
+            if (windowsIdentity.IsAuthenticated)
+            {
+                // Add custom NetworkUser role.
+                roles[0] = "NetworkUser";
+            }
+
+            if (windowsIdentity.IsGuest)
+            {
+                // Add custom GuestUser role.
+                roles[1] = "GuestUser";
+            }
+
+            if (windowsIdentity.IsSystem)
+            {
+                // Add custom SystemUser role.
+                roles[2] = "SystemUser";
+            }
+
+            // Construct a GenericIdentity object based on the current Windows
+            // identity name and authentication type.
+            string authenticationType = windowsIdentity.AuthenticationType;
+            string userName = windowsIdentity.Name;
+            GenericIdentity genericIdentity =
+                new GenericIdentity(userName, authenticationType);
+
+            // Construct a GenericPrincipal object based on the generic identity
+            // and custom roles for the user.
+            GenericPrincipal genericPrincipal =
+                new GenericPrincipal(genericIdentity, roles);
+
+            return genericPrincipal;
+        }
+
+
+        private List<GroupPrincipal> GetGroups(string userName)
+        {
+            List<GroupPrincipal> result = new List<GroupPrincipal>();
+
+            // establish domain context
+            PrincipalContext yourDomain = new PrincipalContext(ContextType.Domain);
+
+            // find your user
+            UserPrincipal user = UserPrincipal.FindByIdentity(yourDomain, userName);
+
+            // if found - grab its groups
+            if (user != null)
+            {
+                PrincipalSearchResult<Principal> groups = user.GetAuthorizationGroups();
+
+                // iterate over all groups
+                foreach (Principal p in groups)
+                {
+                    // make sure to add only group principals
+                    if (p is GroupPrincipal)
+                    {
+                        result.Add((GroupPrincipal)p);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
+        private static string createJWT(string username, string[] groups, string name)
+        {
+            //string[] groups_ = { "Volvo", "BMW", "Ford", "Mazda" };
+            var payload = new Dictionary<string, object>
+            {
+                { "username", username.Replace("\\\\","\\" )},
+                 { "id", username },
+                 { "name", name },
+                { "groups",  groups } 
+        
+            };
+
+         
+
+             string secret = ConfigurationManager.AppSettings["jwtSecret"];
+
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+
+            var token = encoder.Encode(payload, secret);
+            return token;
+        }
+
+
+        private static string getTicket(string jwt, string ignoreSSLError, string url, string path)
+        {
+            var client = new RestClient(url +path + "/ticket");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("authorization", "Bearer " + jwt);
+            request.AddHeader("content-type", "application/json");
+
+            if (ignoreSSLError != "false")
+            {
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            }
+            IRestResponse response = client.Execute(request);
+            return response.Content;
+        }
+
+
+
+
+
+
+    }
+}
